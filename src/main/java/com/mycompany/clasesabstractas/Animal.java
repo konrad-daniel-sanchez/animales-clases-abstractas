eb4301fb5a0c996911ca5eabfe8995087f8d14a7
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-05-09 09:01:13 -0500

    Se adiciona la clase abstracta Animal.

---
+++ src/main/java/com/mycompany/clasesabstractas/Animal.java
@@ -0,0 +1,9 @@
+package com.mycompany.clasesabstractas;
+
+public abstract class Animal {
+    public String comer(){
+        return "Estoy comiendo";
+    }
+    
+    public abstract String hacerSonido();
+}
